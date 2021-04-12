201b759699a0d23d65e641df33f08fa4d572f360
kehinde afusat <adeniyekehinde0@gmail.com>
2021-04-12 12:12:15 +0000

    todolist

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import './style.css'
-import { sampleApp } from './modules/sampleApp'
-import { defaultSetup } from './modules/control'
-import { restoreAppData, dataInStorage, storeAppData } from './modules/storage'
+import StorageController from './js_modules/controllers/StorageController';
 
-function init() {
-    let app
-    if (dataInStorage()) {
-        app = restoreAppData()
-    } else {
-        app = sampleApp()
-        storeAppData(app)
-    }
-    defaultSetup(app)
-}
+const storageController = new StorageController(localStorage);
+storageController.init();
 
-init()
+// responsive
+document.querySelector('.menuBtn').addEventListener('click', () => {
+  const menuView = document.querySelector('.projects-menu');
+  menuView.classList.toggle('display-flex');
+});
